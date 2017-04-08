b4c4a73ba1e0a5b98864b861af048ac9f6beb753
vishalgautamm <vishal.g@live.ca>
2017-04-08 17:41:59 -0400

    Implemented searchSpotify until function

--- src/components/app.js
+++ src/components/app.js
@@ -12,7 +12,7 @@ export default class App extends Component {
 
   fetchSongs = () => {
     console.log('Hey I\'m clicked');
-  }
+  };
   render() {
     const { initialMessage } = this.state;
     return (
