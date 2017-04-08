3b913a1a4102dd064547a652912ae8b13f1172df
vishalgautamm <vishal.g@live.ca>
2017-04-08 19:12:12 -0400

    SongItem component - completed

---
+++ src/components/SongItem/SongItem.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+import styles from './SongItem.css';
+
+const SongItem = (props) => {
+  console.log(props);
+  return (
+    <div> SongItem </div>
+  );
+};
+
+export default SongItem;
--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import searchSpotify from '../utils/searchSpotify';
+import SongItem from './SongItem/SongItem';
 
 export default class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       initialMessage: 'greeting',
       song: '',
+      tracks: {},
     };
   }
 
   fetchSongs = () => {
     searchSpotify(this.state.song)
-      .then(songs => console.log(songs))
+      .then(({ tracks }) => this.setState({ tracks }));
   };
+
   render() {
-    const { initialMessage } = this.state;
+    const { tracks } = this.state;
+    console.log('Tracks: ', tracks);
     return (
       <div>
         <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
+        {tracks.items && <SongItem songData={tracks.items[0]} />}
       </div>
     );
   }
